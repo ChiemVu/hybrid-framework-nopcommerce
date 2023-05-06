6389774486962a39ad4cf82835c89ac75fbe0e76
ChiemVu <chiemvt.ict@gmail.com>
2023-05-07 06:48:13 +0700

    Init Automation Framework

---
+++ testcases/com/nopcommerce/user/User_04_Invoice.java
@@ -0,0 +1,24 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_04_Invoice {
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+  @Test
+  public void f() {
+  
+  int a = 10;
+  System.out.println(a);
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  
+  }
+
+}
---
+++ testdata/com/nopcommerce/data/UserData.java
@@ -0,0 +1,18 @@
+package com.nopcommerce.data;
+
+public class UserData {
+	// Tạo ra các sub class để quản lý data test cho từng class 
+	public class Register{
+		
+	}
+	public class Login{
+		
+	}
+	public class Adrress{
+		
+	}
+	public class MyAccount{
+		
+	}
+	
+}
